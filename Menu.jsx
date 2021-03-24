f32f7c618fba7441af1e0ffcc4c204f088388f7a
Bhupendra Gautam <bhupendragtm777@gmail.com>
2021-03-24 15:41:49 +0545

    Main Menu

--- Menu.jsx
+++ Menu.jsx
@@ -1,4 +1,3 @@
-import react from 'react';
 import './menu.css';
 
 function Menu() {
--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Works from './Works';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Works />
   </React.StrictMode>,
   document.getElementById('root')
 );
