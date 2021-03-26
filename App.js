c96945df9d9b1c3e20a0e9d97bdf7a895f624900
bhupendragth <bhupendragtm777@gmail.com>
2021-03-26 13:03:31 +0545

    Data Array Mapped in JSX

---
+++ App.js
@@ -0,0 +1,14 @@
+import './index.css';
+import Menu from './Menu';
+import Work from './Work';
+import Soc_data from './Soc.jsx';
+
+function App() {
+  return (<>
+    <Menu />
+    <Work />
+    <Soc_data />
+    </>
+    );}
+
+export default App;
